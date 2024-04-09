217e80ddb91e0c17441ec6b2b2d30b5e15ed9ce8
Jordan Hoey <jordan.hoey@justice.gov.uk>
2024-04-09 16:18:19 +0100

    testing

--- scripts/common/get-slack-name.sh
+++ scripts/common/get-slack-name.sh
@@ -3,4 +3,4 @@ source scripts/common/common-functions.sh
 
 approver_slack_name=$(get_slack_displayname_from_github_username $APPROVER_GITHUB_NAME)
 
-echo APPROVER_SLACK_NAME=$approver_slack_name >>$GITHUB_ENV
+echo "APPROVER_SLACK_NAME=$approver_slack_name" >> $GITHUB_ENV
