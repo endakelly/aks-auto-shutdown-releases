bd2c3eb9da71e6aaafcf715b2cdbfd1abd2ef0b9
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-04-10 11:31:52 +0100

    call function, remove step from action

--- scripts/remove_entry.py
+++ scripts/remove_entry.py
@@ -12,19 +12,21 @@ def remove_entry(issue_number):
             for i, entry in enumerate(data):
                 if entry.get("issue_link") == f"https://github.com/hmcts/aks-auto-shutdown-releases/issues/{issue_number}":
                     del data[i]
-                    break
+                    # Write the modified data back to the file
+                    with open(filepath, "w") as json_file:
+                        json.dump(data, json_file, indent=4)
+                    print(f"Entry associated with issue {issue_number} removed successfully.")
+                else:
+                    print(f"Entry for issue {issue_number} was not found")
+                break
             
-        # Write the modified data back to the file
-        with open(filepath, "w") as json_file:
-            json.dump(data, json_file, indent=4)
-        
-        print(f"Entry associated with issue {issue_number} removed successfully.")
-    
     except FileNotFoundError:
         print("issues_list.json file not found.")
     
     except Exception as e:
         print(f"An error occurred: {e}")
+
+remove_entry(sys.argv[1])
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
